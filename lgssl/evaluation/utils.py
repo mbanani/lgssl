e58da46286012c5b7bdb0b3eeaf99709033f595f
Mohamed El Banani <mbanani@umich.edu>
2023-03-02 12:06:39 -0500

    all models gets moved to device in line 65, so no need to do it twice

--- lgssl/evaluation/utils.py
+++ lgssl/evaluation/utils.py
@@ -49,7 +49,7 @@ def get_model(model_name, ckpt_name):
         model.fc = torch.nn.Identity()
     elif model_name == "clip":
         assert model_name == "resnet50"
-        model, _ = clip.load("RN50", device=device)
+        model, _ = clip.load("RN50", device="cpu")
         model = model.visual
     elif model_name == "lgssl_checkpoints":
         ckpt_path = Path(__file__) / f"../../../data/checkpoints/{ckpt_name}.ckpt"
